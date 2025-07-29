6c0fcd7e05cd9396cfe77ea118cf48e86ab738c4
jasu55 <jasu14707@gmail.com>
2025-07-29 12:36:01 +0800

    array-method

---
+++ array-map-filter-sort.js
@@ -0,0 +1,78 @@
+// map method
+// array map arrayiig ooriig ni oorchlohgvi
+// map neg function awna , tuhain fucntion bas neg utga awna
+// map method (for loop)=  shine array vvsgej ogno , shine array ni huuchin arraytai length ijil bn
+// ymar array vvsgehee map iin return eer shiidne
+
+let ages = [10, 203, 40, 30, 50];
+let mapAges = ages.map((age) => {
+  return { value: 10 * age };
+});
+console.log("mapAges", mapAges);
+
+let data = [
+  { name: "boldo", age: 20, grade: 11, gender: "male" },
+  { name: "dorjo", age: 20, grade: 11, gender: "male" },
+  { name: "tsetsge", age: 20, grade: 11, gender: "female" },
+  { name: "dulma", age: 15, grade: 11, gender: "female" },
+];
+
+let newStudents = data.map((student) => {
+  return { name: student.name, age: student.age, grade: student.grade + 1 };
+});
+console.log("newStudent", newStudents);
+
+//filter
+//filter ooriig ni oorchlohgvi
+// shine array vvsgej ogno
+// filter method neg function avna, tuhain fucntion neg utga avna
+// return deer true bh bvh elementiig tsugluuluuad butsaana
+
+let filterAge = ages.filter(() => {});
+
+// sort method
+// sort metho function avna
+// avjgaa fucntion 2 utga orj irne
+// return deer + esvel - too butsaana
+
+ages.sort((a, b) => {
+  return false;
+});
+
+// nasand hvrsen suragchdiig filterlej oloh function bich
+const findAdultStudents = (arr) => {
+  let adult = arr.filter((student) => {
+    return student.age > 17;
+  });
+  return adult;
+};
+let resultAdult = findAdultStudents(data);
+console.log("adult", resultAdult);
+
+// gender ogongvvt tuhain gendereer filterlej ogoh function bich
+
+const findGender = (arr, gender) => {
+  let filteredByGender = arr.filter((student) => {
+    return student.gender === gender;
+  });
+  return filteredByGender;
+};
+let resultByGender = findGender(data, "male");
+console.log("filteredByGender=", resultByGender);
+
+// nasaar n sortloh function bich
+
+const sortByAge = (arr) => {
+  arr.sort((student2, student1) => {
+    if (student2.name > student1.name) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+};
+
+// neg too ogonguut tuhain toonoos ih balanactei surgchdiig yalgaj ogoh function bich
+// classCode gesen field nemeh function bich
+// eg: addClassCodeToStudents(students,"3A") => [{name: "boldo",age: 3,grade: 11,gender: "male",balance: 1231,classCode:"3A"},...]
+// removeGenders from student array function bich
